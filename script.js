d3c5deb0dcdd32fa229118c93162058bd7cf4630
Hannah Bjorklund <bjorklundhannah@gmail.com>
2023-10-31 10:00:03 -0500

    Completed the render function, which changes the DOM to reflect the changes to hp, ap, and fungus state

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 // State Variables can be declared outside of the onReady
-// Feel free to make this to what you want!
-// Example:
-
 let characterAP = 100;
 let fungusHP = 100;
 
@@ -31,34 +28,13 @@ let attacks = [
 
 function onReady() {
     console.log("Ready to go!")
-    
-    // Make sure you check the index.html file! 
-    // There are lots of buttons and things ready for you to hook into here!
-    
-    
-    // 🧠 Remember
-    // - Handle events that ->
-    // - Updates state which is ->
-    // - Rendered to the DOM
 }
-
-// Clicking a button
-// The button corresponds to attacks in the attack array
-// Get the apCost and hpDamage for that attack from the array
-
-// Check to make sure we have enough AP to do that attack
-//  if yes, then attack and update AP meter
-//  if ap hits zero and the fungus is still alive, then monster gets the 'jump' class and we lose
-//  if not enough AP to do an attack, the button is disabled
-
-//  Check the fungus's hp
-//      if hp will fall below 50, then it regenrates 1 hp every second
-//      if hp will hit 0, the fungus is dead. We have to give it the 'dead' class
 
 function makeAttack(event, attackName){
     console.log("Using", attackName);
     // Find the object with the attack name
     let attack = attacks.find(attack => attack.name === attackName);
+    
     // Get the apCost and the hpDamage for the attack
     let apCost = attack.apCost;
     let hpDamage = attack.hpDamage;
@@ -82,9 +58,11 @@ function makeAttack(event, attackName){
     console.log("Character AP:", characterAP);
     console.log("Fungus HP:", fungusHP);
 
+    // Render changes
     render();
 }
 
+// Mushroom regenerates HP
 function regen(){
     if(fungusHP < 50 && fungusHP > 0){
         fungusHP++;
@@ -106,14 +84,19 @@ function render(){
     hpText.innerHTML = `${fungusHP} HP`;
 
     // Render changes to win state
+    let fungus = document.getElementById('freaky-fungus');
+    let buttons = document.getElementsByClassName('attack-btn');
     if(fungusHP == 0){
-        console.log("Fungi died");
-        document.getElementById('freaky-fungus').classList.add('freaky-fungus.dead');
+        // Fungus dies
+        fungus.classList.replace('walk', 'dead');
     } else if(characterAP == 0){
-        console.log("You lost to fungus");
-        document.getElementById('freaky-fungus').classList.add('freaky-fungus.jump');
+        // Fungus wins
+        fungus.classList.replace('walk', 'jump');
         // Disable buttons
-        document.getElementsByName(button).setAttribute('disabled');
+        for(let button of buttons){
+            button.setAttribute('disabled', true);
+        }
+        
     }
 }
 
